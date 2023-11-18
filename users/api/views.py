1f942d540a98ca01bc15fd83e2e47d40278ca912
hugo_quintero <hugo.quintero@udea.edu.co>
2023-11-18 15:00:10 -0500

    Corrección de segurida encriptación de password

--- users/api/views.py
+++ users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from users.models import user
 from users.api.serializers import UserSerializer
@@ -8,3 +9,16 @@ class UserApiViewSet (ModelViewSet):
     permission_class = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = user.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+
+        return super().create(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        password = request.data['password']
+        if password:
+            request.data['password'] = make_password(password)
+        else:
+            request.data['password'] = request.user.password
+        return super().update(request, *args, **kwargs)
